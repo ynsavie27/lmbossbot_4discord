fdb7beb8f1879f1b4a6db6a30b956a2d61287e64
ynsavie27 <70462189+ynsavie27@users.noreply.github.com>
2020-09-06 20:43:19 +0900

    Add files via upload

---
+++ send_msg.py
@@ -0,0 +1,16 @@
+#discordのライブラリをインポート
+import discord
+#自分のトークンにしてね
+TOKEN = 'NzQ5NDkzNjMzNTQ3NTAxNTY5.X0syVw.Zh95SnG5-FVQUPy5vz_GeNmFKH0'
+
+# 接続に必要なオブジェクトを作る
+client = discord.Client()
+
+#BOTが起動したとき
+@client.event
+async def on_ready():
+    print('寝てた')
+    print(client.guilds)
+
+#BOTの起動
+client.run(TOKEN)
