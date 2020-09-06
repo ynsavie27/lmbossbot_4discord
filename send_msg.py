87db4c5952725f6f32a6ab62e74a75aceea8a753
ynsavie27 <70462189+ynsavie27@users.noreply.github.com>
2020-09-06 21:37:51 +0900

    Add files via upload

--- send_msg.py
+++ send_msg.py
@@ -2,17 +2,19 @@
 import discord
 #自分のトークンにしてね
 TOKEN = 'NzQ5NDkzNjMzNTQ3NTAxNTY5.X0syVw.Zh95SnG5-FVQUPy5vz_GeNmFKH0'
+CHANNEL_ID = 751985875868581888
 
 # 接続に必要なオブジェクトを作る
 client = discord.Client()
 
+async def send_msg()
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('hello')
 
 #BOTが起動したとき
 @client.event
 async def on_ready():
-    print('寝てた')
-    print(client.guilds)
-    
+    await send_msg()
 
 #BOTの起動
 client.run(TOKEN)
