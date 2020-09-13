1d22b32f548261e6460aed1dece4617f4fa70596
ynsavie27 <70462189+ynsavie27@users.noreply.github.com>
2020-09-13 14:49:54 +0900

    Add files via upload

---
+++ cre_db.py
@@ -0,0 +1,7 @@
+import sqlite3
+#discordのライブラリをインポート
+import discord
+
+conn = sqlite3.connect('lmbbot.sqlite3')
+c = conn.cursor()
+c.execute('create table bosspop(No_ INTEGER PRIMARY KEY, Ch_ID TEXT, Boss_ID TEXT, Pop_Time TEXT, AddText TEXT, MsgSendFlg INTEGER, DisableFlg INTEGER)')
