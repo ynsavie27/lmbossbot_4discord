d0e59360fb99f73d46c4d2d0f42946fdaaedf1a2
ynsavie27 <70462189+ynsavie27@users.noreply.github.com>
2020-09-13 20:41:03 +0900

    Add files via upload

--- lmbbot.py
+++ lmbbot.py
@@ -25,8 +25,8 @@ async def fetch_popdata():
         send_channel = client.get_channel(row[1])
         await send_channel.send(row[2] + 'pop ' + str(row[3])[6:])
         c.execute("UPDATE bosspop SET MsgSendFlg = 1 WHERE No_ = ?", (row[0],))
-        conn.commit
-    # conn1.close
+        conn.commit()
+    # conn1.close()
 
 #BOTが起動したとき
 @client.event
@@ -157,14 +157,19 @@ async def on_message(message):
 
             #DB書き込み
             c.execute("INSERT INTO bosspop(Ch_ID, Boss_ID, Pop_Time, AddText, MsgSendFlg, DisableFlg) VALUES (?, ?, ?, ?, ?, ?)", (ch_id, boss_id, pop_time, addtext, msgsendflg, disableflg))
-            conn.commit
+            # conn.commit()
+            try:
+                conn.commit()
+            except sqlite3.Error as er:
+                print(er.message)
+
             # for row in c.execute('SELECT * FROM bosspop'):
             #     print(row)
             
             await message.channel.send(bname + ' Next Pop ' + poptime.strftime("%Y/%m/%d %H:%M") + rand)
             #print(str(message.channel.id))
 
-            # conn.close
+            # conn.close()
 
         else:
             return
