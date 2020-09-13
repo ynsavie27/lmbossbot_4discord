5fd4d28b8170b20b12cf3b91ec873c2cf896e022
ynsavie27 <70462189+ynsavie27@users.noreply.github.com>
2020-09-13 17:48:36 +0900

    Add files via upload

--- listup_bosspop.py
+++ listup_bosspop.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conn = sqlite3.connect('lmbbot.sqlite3')
 c = conn.cursor()
-c.execute('SELECT * FROM bosspop')
+c.execute("SELECT * FROM bosspop")
 res = c.fetchall()
 print(len(res))
 for row in res:
